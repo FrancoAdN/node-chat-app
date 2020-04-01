52830601ae3a56d6e233026c6d58e96e71e3de0e
FrancoAdN <francoadinapoli@gmail.com>
2020-04-01 16:34:17 -0300

    add

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,5 @@
 
-const socket = io('ws://localhost:1234', { transports: ['websocket', 'polling'] })
+const socket = io('http://localhost:1234', { transports: ['websocket', 'polling'] })
 
 socket.emit('server')
 
